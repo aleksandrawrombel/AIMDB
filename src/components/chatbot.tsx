78fb62c7d87d7a8c31d1e1ea3b27003cb4e8e01c
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-04-04 15:26:50 +0200

    answer and author comparison logic added

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -2,6 +2,7 @@ import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
 import humanoid_icon from "../assets/humanoid_icon.svg";
 import user_icon from "../assets/user_icon.svg";
+import { randomMoviesArray } from "../assets/movies";
 
 import { useState, useEffect } from "react";
 
@@ -13,10 +14,33 @@ function GetName({ steps }: { steps: { name: { value: string } } }) {
   }, [steps.name.value]);
 
   return (
-    <p>{`I am the AI Movie Database main manager, the one and only manager in fact, and I have a little problem. Would you like to help me, ${name}?`}</p>
+    <p>{`I am the AI Movie Database main manager, the one and only manager in fact. I have a little problem... Would you like to help me, ${name}?`}</p>
   );
 }
 
+interface AnswerCheckerProps {
+  index: number;
+  previousStep: { value: string };
+}
+
+function AnswerChecker({ index, previousStep }: AnswerCheckerProps) {
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  useEffect(() => {
+    const userAnswer = previousStep.value;
+    const currentMovieAuthor = randomMoviesArray[index].author;
+
+    if (userAnswer === currentMovieAuthor) {
+      setIsCorrect(true);
+    }
+  }, [index, previousStep.value]);
+
+  return (
+    <>
+      <p>{isCorrect ? "Yes, you are right! Let's keep going, I am so thrilled!" : "No, no, no! It's not correct, let's try another one!"}</p>
+    </>
+  );
+}
 function Chatbot() {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -59,7 +83,7 @@ function Chatbot() {
     },
     {
       id: "username",
-      message: "Hi {previousValue}, nice to meet you!",
+      message: "Hello {previousValue}, nice to meet you!",
       trigger: "explanation",
     },
     {
@@ -71,8 +95,8 @@ function Chatbot() {
     {
       id: "help",
       options: [
-        { value: "yes", label: "Sure!", trigger: "yes" },
-        { value: "no", label: "No, thanks...", trigger: "no_thanks" },
+        { value: "yes", label: "Of course!", trigger: "yes" },
+        { value: "yes2", label: "Yes, I guess...", trigger: "yes2" },
       ],
     },
     {
@@ -81,13 +105,71 @@ function Chatbot() {
       trigger: "explanation_help",
     },
     {
-      id: "no_thanks",
-      message: "Well, ok then... Bye!",
-      end: true,
+      id: "yes2",
+      message: `Great, let's go!`,
+      trigger: "explanation_help",
     },
     {
       id: "explanation_help",
-      message: "Let me tell you all about it!",
+      message:
+        "I have some movie descriptions that I am having trouble with. Are those movies written by humans or are they written by AI? Could you help me check? Here comes the first description! Is it a human movie or an AI movie?!",
+      trigger: "firstMovie",
+    },
+    {
+      id: "firstMovie",
+      message: `${randomMoviesArray[0].description}`,
+      delay: 6000,
+      trigger: "firstAIOrHuman",
+    },
+    {
+      id: "firstAIOrHuman",
+      options: [
+        { value: "AI", label: "AI", trigger: "firstAnswerChecker" },
+        { value: "human", label: "Human", trigger: "firstAnswerChecker" },
+      ],
+    },
+    {
+      id: "firstAnswerChecker",
+      component: <AnswerChecker index={0} />,
+      asMessage: true,
+      trigger: "secondMovie",
+    },
+    {
+      id: "secondMovie",
+      message: `${randomMoviesArray[1].description}`,
+      delay: 6000,
+      trigger: "secondAIOrHuman",
+    },
+    {
+      id: "secondAIOrHuman",
+      options: [
+        { value: "AI", label: "AI", trigger: "secondAnswerChecker" },
+        { value: "human", label: "Human", trigger: "secondAnswerChecker" },
+      ],
+    },
+    {
+      id: "secondAnswerChecker",
+      component: <AnswerChecker index={1} />,
+      asMessage: true,
+      trigger: "thirdMovie",
+    },
+    {
+      id: "thirdMovie",
+      message: `${randomMoviesArray[2].description}`,
+      delay: 6000,
+      trigger: "thirdAIOrHuman",
+    },
+    {
+      id: "thirdAIOrHuman",
+      options: [
+        { value: "AI", label: "AI", trigger: "thirdanswerChecker" },
+        { value: "human", label: "Human", trigger: "thirdanswerChecker" },
+      ],
+    },
+    {
+      id: "thirdanswerChecker",
+      component: <AnswerChecker index={2} />,
+      asMessage: true,
       end: true,
     },
   ];
@@ -96,18 +178,24 @@ function Chatbot() {
     background: "#f5f8fb",
     fontFamily: "Helvetica Neue",
     headerBgColor: "#333333",
-    headerFontColor: "#fff",
+    headerFontColor: "#ffffff",
     headerFontSize: "1.5rem",
     botBubbleColor: "#333333",
-    botFontColor: "#fff",
+    botFontColor: "#ffffff",
     userBubbleColor: "#ffdb4d",
     userFontColor: "#333333",
+  };
+
+  const bubbleOptionStyle = {
+    backgroundColor: "rgba(255, 219, 77, 0.6)",
+    color: "#333333",
   };
 
   return (
     <main className={`chatbot_container ${isVisible ? "fade_in" : ""}`}>
       <ThemeProvider theme={theme}>
         <ChatBot
+          className="chatbot"
           steps={steps}
           width={"60vw"}
           height={"60vh"}
@@ -119,8 +207,9 @@ function Chatbot() {
           }
           botAvatar={humanoid_icon}
           userAvatar={user_icon}
-          botDelay={1700}
-          userDelay={1200}
+          botDelay={2500}
+          userDelay={10}
+          bubbleOptionStyle={bubbleOptionStyle}
         />
       </ThemeProvider>
     </main>
